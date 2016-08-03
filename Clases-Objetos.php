29a0314612e40f7556f9a8cf3957c9618d82e227
user.email <cnunez@icesi.edu.co>
2016-08-03 09:19:14 -0500

    Practica CLASES_OBJETOS

---
+++ Clases-Objetos.php
@@ -0,0 +1,16 @@
+<?php
+
+class Persona{
+	//Atributos
+	public $nombre="pedro";
+
+	//Metodos
+	public function hablar($mensje){
+		echo mensaje;
+	}
+}
+
+$persona = new Persona();
+echo $persona->nombre; 
+
+?>
